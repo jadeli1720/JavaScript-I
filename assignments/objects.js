b545443ff0bac6b4cf281ba0440138d141e6c1ab
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-10 14:05:55 -0600

    Modified and completed challenges 1 and 2 of objects.js. Moving on to 3rd challenge

--- assignments/objects.js
+++ assignments/objects.js
@@ -15,23 +15,63 @@ const example = {
   "name": "Example",
   "email": "examples@you.edu",
   "gender": "F"
-}
+};
 
 // Write your intern objects here:
+
+const Mitzi = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+};
+
+const Kennan = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+};
+
+const Keven = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia",
+  "gender": "M"
+};
+
+const Gannie = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+};
+
+const Antonietta = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+};
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(Object.values(Mitzi)[1]);
 
 // Kennan's ID
+console.log(Object.values(Kennan)[0]);
 
 // Keven's email
+console.log(Object.values(Keven)[2]);
 
 // Gannie's name
+console.log(Object.values(Gannie)[1]);
 
 // Antonietta's Gender
+console.log(Object.values(Antonietta)[3]);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
